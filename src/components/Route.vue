cc7f527b96f01e8c487c2220ae821dc2104866d1
siaochanwu <jill007tw@gmail.com>
2021-11-24 16:58:58 +0800

    點擊公車班次後畫面呈現及抓站牌名稱

--- src/components/Route.vue
+++ src/components/Route.vue
@@ -1,8 +1,29 @@
 <template>
-    <div>
-        <div>
-            <button>去程</button>
-            <button>回程</button>
+    <div class="bg-gray-100 w-full">
+        <Nav />
+        <div class="bg-indigo-600 rounded-b-lg py-3 px-6">
+            <p class="text-left bg-blue-100 text-indigo-600 rounded-full px-2 w-3/12 mb-3">{{ country }}</p>
+            <div class="flex justify-between items-baseline">
+                <h1 class="text-white text-3xl font-bold">{{ routeName }}</h1>
+                <div>123</div>
+            </div>
+        </div>
+        <div class="mt-5 w-12/12 md:w-6/12 flex mx-auto p-3 items-center">
+            <button class="w-6/12 bg-gray-200 text-black p-2 rounded-xl mx-5" @click="direction = 0" :class="{ 'bg-indigo-600 text-white': direction == 0 }">往{{ goWhere }}</button>
+            <i class="fas fa-arrows-alt-h text-3xl text-gray-400"></i>
+            <button class="w-6/12 bg-gray-200 text-black p-2 rounded-xl mx-5" @click="direction = 1" :class="{ 'bg-indigo-600 text-white': direction == 1 }">往{{ backWhere }}</button>
+        </div>
+        <div v-if="direction == 0" class="">
+            <div v-for="item in go" :key="item.StopID" class="w-10/12 md:w-6/12 mx-auto bg-white my-2 p-3 rounded-xl text-left">
+                <span class="bg-yellow-300 px-2 py-1 mx-4 rounded-full">進站中</span>
+                <span class="font-bold">{{ item.StopName.Zh_tw }}</span>
+            </div>
+        </div>
+        <div v-else>
+            <div v-for="item in back" :key="item.StopID" class="w-10/12 md:w-6/12 mx-auto bg-white my-2 p-3 rounded-xl text-left">
+                <span class="bg-yellow-300 px-2 py-1 mx-4 rounded-full">進站中</span>
+                <span class="font-bold">{{ item.StopName.Zh_tw }}</span>
+            </div>
         </div>
     </div>
 </template>
@@ -11,18 +32,39 @@
 import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex'
-import jsSHA from "jssha";
+import jsSHA from "jssha"
+import Nav from './Nav.vue'
 
 export default {
+    components: { Nav },
     setup() {
+        interface info {
+            RouteName: {
+                Zh_tw: string,
+                
+            },
+            Direction: number,
+            Stops: busStop[]
+        }
+        interface busStop {
+            StopName: {
+                Zh_tw: string
+            },
+            StopID: string,
+            StationID: string
+        }
+
         const route = useRoute()
         const store = useStore()
 
         const routeName = ref<any>(route.params.routeName)
         const country = ref(store.state.country)
-        const go = ref('')
-        const back = ref('')
-        const filterData = ref([])
+        const go = ref<busStop[]>([])
+        const goWhere = ref('')
+        const back = ref<busStop[]>([])
+        const backWhere = ref('')
+        const filterData = ref<info[]>([])
+        const direction = ref(0)
 
         function getAuthorizationHeader() {
             let AppID = import.meta.env.VITE_APP_ID;
@@ -54,8 +96,25 @@ export default {
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                filterData.value = data.filter(item => item.RouteName.Zh_tw == routeName)
-                console.log(filterData.value)
+                const busData = <info[]>data
+                filterData.value = busData.filter(item => item.RouteName.Zh_tw == routeName)
+                const aaa = filterData.value.forEach(item => {
+                    //0:去程 1:返程
+                    if (item.Direction == 0) {
+                        for (let i = 0; i < item.Stops.length; i++) {
+                            go.value.push(item.Stops[i])
+                            goWhere.value = item.Stops[i].StopName.Zh_tw
+                            console.log(goWhere.value)
+                        }
+                    } else {
+                        for (let i = 0; i < item.Stops.length; i++) {
+                            back.value.push(item.Stops[i])
+                            backWhere.value = item.Stops[i].StopName.Zh_tw
+                            console.log(backWhere.value)
+                        }
+                    }
+                })
+                console.log('go', go.value)
             })
         }
 
@@ -69,7 +128,10 @@ export default {
             country,
             go,
             back,
-            filterData
+            filterData,
+            direction,
+            goWhere,
+            backWhere
         }
     }
 }
